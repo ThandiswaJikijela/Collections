c0988592136805b597c14c6f46b0a8144b73c234
ThandiswaJikijela <36195934+ThandiswaJikijela@users.noreply.github.com>
2019-03-08 09:46:22 +0200

    Update TestCollectionsInterface.java

--- Collections/src/test/java/ac/za/cput/project2/TestCollectionsInterface.java
+++ Collections/src/test/java/ac/za/cput/project2/TestCollectionsInterface.java
@@ -8,17 +8,26 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class TestCollectionsInterface {
+   private CollectionsInterface collection;
+
+    @Before
+    public void setUp() throws Exception{
+        collection = new CollectionsInterface();
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
     @Test
     public void testCollection()
     {
         //testing if the word Set is on the list, and counting how many elements that are in the list
-        Collection<String> collection = new HashSet<String>();
-        collection.add("List");
-        collection.add("Set");
-        collection.add("Map");
-        collection.add("Queue");
+        Collection<String> collections = collection.Collection();
 
-        assertTrue(collection.contains("Set"));
+        assertTrue(collections.contains("Set"));
         assertEquals("Number of collections are 4:" , 4,4);
     }
     @Test
@@ -27,14 +36,7 @@ public class TestCollectionsInterface {
         /*testing if the if ziyanda mapinda exists on the list,
         * and checking if index number 2 has been removed and replaced by another name
         */
-        List<String> actors = new ArrayList<>();
-        actors.add("Ziyanda Mapinda");
-        actors.add("Thandie Jicks");
-        actors.add("Luyolo Ndiki");
-        actors.add("Sino Ranana");
-        actors.add("Jack Braid");
-        actors.remove(2);
-        actors.set(2, "Brad Pet");
+        List<String> actors = collection.List();
 
         assertTrue(actors.contains("Ziyanda Mapinda"));
         assertTrue(actors.contains("Brad Pet"));
@@ -44,12 +46,7 @@ public class TestCollectionsInterface {
     public void testSet()
     {
         //testing if the element Fire is on the list, and counting how many elements that are in the list
-        Set<String> elements = new HashSet<>();
-        elements.add("Fire");
-        elements.add("Ice");
-        elements.add("Air");
-        elements.add("Earth");
-        elements.add("Fire");
+        Set<String> elements = collection.Set();
 
         assertTrue(elements.contains("Fire"));
         assertEquals("Number of elements are 4:" , 4,4);
@@ -59,11 +56,7 @@ public class TestCollectionsInterface {
     public void testMap()
     {
         // testing how many inputs are in the list
-        Map map = new HashMap();
-        map.put(100, "Red");
-        map.put(101, "Blue");
-        map.put(102, "Pink");
-        map.put(103, "Green");
+        Map map = collection.Map();
 
         assertEquals("Map ID",4, 4);
     }
